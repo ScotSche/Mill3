fe4ac51a49dbf232029e57778d4f7273410b18d9
Philipo289 <56684600+Philipo289@users.noreply.github.com>
2021-01-09 14:05:12 +0100

    Observer changed to Scala3

--- src/main/scala/Util/Observer.scala
+++ src/main/scala/Util/Observer.scala
@@ -1,11 +1,11 @@
 package Util
 
-trait Observer {
+trait Observer: 
   def update: Unit
   def updatePlayer: Unit
-}
 
-class Observable {
+
+class Observable:
   var subscribers: Vector[Observer] = Vector()
 
   def add(s: Observer): Unit = subscribers = subscribers :+ s
@@ -14,4 +14,3 @@ class Observable {
 
   def notifyObservers: Unit = subscribers.foreach(o => o.update)
   def notifyPlayerObserver: Unit = subscribers.foreach(o => o.updatePlayer)
-}
